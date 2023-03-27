bea5213b506f6872dbfbd5a9f949ef608e731c87
Syst3mR0ot <ahmedsgk@gmail.com>
2023-03-27 12:26:30 +0200

    create project files

---
+++ src/test.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    string name;
+    cout << "Please Enter your first Name \n";
+    cin  >> name;
+    cout << "Welcome " << name << endl;
+    return 0;
+}
